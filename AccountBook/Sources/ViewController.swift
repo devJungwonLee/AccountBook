5e921d4cdf207ad354c32bec91df3192944cd287
Jungwon Lee <dev.jungwon.lee@gmail.com>
2023-04-15 16:19:30 +0900

    [Chore] 프로젝트 초기 설정

---
+++ AccountBook/Sources/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AccountBook
+//
+//  Created by 이정원 on 2023/04/15.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
